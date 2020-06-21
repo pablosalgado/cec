c0e5cd7b9d1bafa4e6f45181a4b589a0b7ae229b
Pablo Salgado <pabloasalgado@gmail.com>
2020-06-20 19:54:39 -0500

    1/MobileNet

--- utils/preprocess_large_mpi_db.py
+++ utils/preprocess_large_mpi_db.py
@@ -34,54 +34,46 @@ test_codes = common.CODES[1:2]
 validation_codes = common.CODES[0:1]
 
 # Collect paths for all PNGs in the MPI directory.
-images_paths = sorted(imutils.paths.list_images(common.MPI_LARGE_DB_PATH))
+image_paths = imutils.paths.list_images(common.MPI_LARGE_DB_PATH)
 
-for k, v in common.LABELS.items():
-    for code in common.CODES:
-        filtered_images_paths = list(
-            filter(
-                lambda image_path: image_path.split(os.path.sep)[-1].startswith(code) and
-                                   image_path.split(os.path.sep)[-2] == v,
-                images_paths
-            )
-        )
+# For each PNG ...
+for image_path in image_paths:
 
-        replace = len(filtered_images_paths) < 64
-        filtered_images_ix = np.sort(
-            np.random.default_rng().choice(len(filtered_images_paths), size=64, replace=replace)
-        )
+    # Get path parts. Last part is the file name, rest are name directories.
+    path_parts = image_path.split(os.path.sep)
+    filename = path_parts[-1]
 
-        for i, ix in enumerate(filtered_images_ix, start=1):
-            image_path = filtered_images_paths[ix]
+    # Decide if the preprocessed image is going to training or testing sets.
+    path_parts.insert(-3, 'datasets')
+    path_parts[-3] = 'cec-data'
 
-            # Get path parts. Last part is the file name, rest are name directories.
-            path_parts = image_path.split(os.path.sep)
-            filename = path_parts[-1]
+    # Build path and filename to save the preprocessed image.
+    save_path = os.path.sep.join(path_parts)
 
-            # Decide if the preprocessed image is going to training or testing sets.
-            path_parts.insert(-3, 'datasets')
-            if code in train_codes:
-                path_parts[-3] = 'cec-train'
-            elif code in test_codes:
-                path_parts[-3] = 'cec-test'
-            else:
-                path_parts[-3] = 'cec-validation'
+    # If preprocessed image exists we don't waste our resources.
+    if os.path.exists(save_path):
+        continue
 
-            # Load MPI image.
-            image = cv2.imread(image_path)
-            faces = common.extract_face(image, 50)
+    # Load MPI image.
+    image = cv2.imread(image_path)
 
-            # To gray scale and resize.
-            face = cv2.cvtColor(faces[0], cv2.COLOR_RGB2GRAY)
-            face = cv2.resize(face, (224, 224))
+    # Extract the face if posible.
+    faces = common.extract_face(image, 50)
 
-            # Create classname directory if necessary.
-            dirs = os.path.sep.join(path_parts[:-1])
-            os.makedirs(dirs, exist_ok=True)
+    # If no face found log the picture path and continue.
+    if len(faces) == 0:
+        print(f'No face detected: {image_path}')
+        continue
 
-            # Save the preprocessed image.
-            path_parts[-1] = f'{code}_{v}_{i:03d}.png'
-            save_path = os.path.sep.join(path_parts)
-            cv2.imwrite(save_path, face)
-            print(f'{image_path} -> {save_path}')
-            faces.clear()
+    # To gray scale and resize.
+    face = cv2.cvtColor(faces[0], cv2.COLOR_RGB2GRAY)
+    face = cv2.resize(face, (224, 224))
+
+    # Create classname directory if necessary.
+    dirs = os.path.sep.join(path_parts[:-1])
+    os.makedirs(dirs, exist_ok=True)
+
+    # Save the preprocessed image.
+    cv2.imwrite(save_path, face)
+    print(f'{image_path} -> {save_path}')
+    faces.clear()
