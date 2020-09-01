811de9356ef40b6be6449d741b2fc4039722bf2e
Pablo Salgado <pabloasalgado@gmail.com>
2020-09-01 17:13:45 -0500

    trial10

--- trial_10.py
+++ trial_10.py
@@ -4,10 +4,11 @@
 # author: Pablo Salgado
 # contact: pabloasalgado@gmail.com
 #
-# https://unir-tfm-cec.s3.us-east-2.amazonaws.com/trial10.tar.gz
+# https://unir-tfm-cec.s3.us-east-2.amazonaws.com/trial-10.tar.gz
 
 import os
 
+import pandas
 import tensorflow as tf
 from keras_video.sliding import SlidingFrameGenerator
 
@@ -15,12 +16,12 @@ import common
 
 # Parameters
 TRIAL = '10'
-BATCH_SIZE = [64]
+BATCH_SIZE = [32]
 TIME_STEPS = [12]
 EPOCHS = 1000
 
 TRL_PATH = f'models/trial-{TRIAL}'
-CLASSES = ['bored', 'confused', 'contempt']
+CLASSES = ['agree_considered', 'agree_continue', 'agree_pure', 'agree_reluctant', 'aha-light_bulb_moment', 'annoyed_bothered', 'annoyed_rolling-eyes', 'arrogant', 'bored', 'compassion', 'confused', 'contempt', 'disagree_considered', 'disagree_pure', 'disagree_reluctant', 'disbelief', 'disgust', 'embarrassment', 'fear_oops', 'fear_terror', 'happy_achievement', 'happy_laughing', 'happy_satiated', 'happy_schadenfreude', 'I_did_not_hear', 'I_dont_care', 'I_dont_know', 'I_dont_understand', 'imagine_negative', 'imagine_positive', 'impressed', 'insecurity', 'not_convinced', 'pain_felt', 'pain_seen', 'remember_negative', 'remember_positive', 'sad', 'smiling_encouraging', 'smiling_endearment', 'smiling_flirting', 'smiling_sad-nostalgia', 'smiling_sardonic', 'smiling_triumphant', 'smiling_uncertain', 'smiling_winning', 'smiling_yeah-right', 'thinking_considering', 'thinking_problem-solving', 'tired', 'treudoof_bambi-eyes']
 
 
 def build_model(time_steps, nout):
@@ -79,10 +80,19 @@ def build_model(time_steps, nout):
 
 def train():
     tf.keras.utils.get_file(
-        fname='cec-videos-augmented.tar.gz',
-        origin='https://unir-tfm-cec.s3.us-east-2.amazonaws.com/cec-videos-augmented.tar.gz',
+        fname='cec-videos-augmented-train-8.tar.gz',
+        origin='https://unir-tfm-cec.s3.us-east-2.amazonaws.com/cec-videos-augmented-train-8.tar.gz',
         extract=True
     )
+
+    tf.keras.utils.get_file(
+        fname='cec-videos-val.tar.gz',
+        origin='https://unir-tfm-cec.s3.us-east-2.amazonaws.com/cec-videos-val.tar.gz',
+        extract=True
+    )
+
+    data = pandas.DataFrame(None, columns=['trial', 'batch_size', 'time_steps', 'cycle', 'files', 'sequences'])
+    data['trial'] = TRIAL
 
     for batch_size in BATCH_SIZE:
         for time_steps in TIME_STEPS:
@@ -98,9 +108,9 @@ def train():
 
             train_idg = SlidingFrameGenerator(
                 classes=CLASSES,
-                glob_pattern=common.HOME + '/.keras/datasets/cec-videos-augmented/{classname}/*.avi',
+                glob_pattern=common.HOME + '/.keras/datasets/cec-videos-augmented-train/{classname}/*.avi',
                 nb_frames=time_steps,
-                split_val=.2,
+                split_val=None,
                 shuffle=True,
                 batch_size=batch_size,
                 target_shape=(224, 224),
@@ -109,22 +119,53 @@ def train():
                 use_frame_cache=False
             )
 
-            validation_idg = train_idg.get_validation_generator()
+            validation_idg = SlidingFrameGenerator(
+                classes=CLASSES,
+                glob_pattern=common.HOME + '/.keras/datasets/cec-videos-val/{classname}/*.avi',
+                nb_frames=time_steps,
+                split_val=None,
+                shuffle=True,
+                batch_size=batch_size,
+                target_shape=(224, 224),
+                nb_channel=3,
+                transformation=data_aug,
+                use_frame_cache=False
+            )
+
+            row = {
+                'trial': TRIAL,
+                'batch_size': batch_size,
+                'cycle': 'training',
+                'time_steps': time_steps,
+                'files': train_idg.files_count,
+                'sequences': len(train_idg.vid_info)
+            }
+            data = data.append(row, ignore_index=True)
+
+            row = {
+                'trial': TRIAL,
+                'batch_size': batch_size,
+                'cycle': 'validation',
+                'time_steps': time_steps,
+                'files': validation_idg.files_count,
+                'sequences': len(validation_idg.vid_info)
+            }
+            data = data.append(row, ignore_index=True)
 
             # Configure callbacks
             callbacks = [
                 tf.keras.callbacks.ModelCheckpoint(
-                    filepath=path + '/model',
+                    filepath=path + '/ckpts/cp-{epoch:04d}.ckpt',
                     monitor='val_accuracy',
-                    mode='max',
-                    save_best_only=True,
+                    # mode='max',
+                    # save_best_only=True,
                     verbose=1,
                 ),
                 tf.keras.callbacks.EarlyStopping(
                     monitor='val_loss',
                     mode='min',
                     verbose=1,
-                    patience=int(EPOCHS * .01)
+                    patience=int(EPOCHS * .02)
                 ),
                 tf.keras.callbacks.CSVLogger(
                     filename=path + '/log.csv'
@@ -146,6 +187,8 @@ def train():
             )
 
             common.plot_acc_loss(history, path + '/plot.png')
+
+    data.to_csv('trial_10.csv')
 
 
 if __name__ == '__main__':
